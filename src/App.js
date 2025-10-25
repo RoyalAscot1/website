42530c0277ebc3b6ad2289c80fea4c0ee3436984
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2025-10-25 19:28:33 +0800

    Added components and tested a navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "assets/theme";
+
+// Material Kit Navbar
+import DefaultNavbar from "./examples/Navbars/DefaultNavbar";
+import routes from "routes";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <DefaultNavbar
+          routes={routes}
+          brand={"hello"}
+          action={{
+            type: "external",
+            route: "https://example.com",
+            label: "Sign Up",
+            color: "info",
+          }}
+        />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+// Routes
+const routes = [
+  { name: "Home", route: "/" },
+  { name: "Upload", route: "/upload" },
+  { name: "Survey", route: "/survey" },
+  { name: "Recommendations", route: "/recommendations" },
+];
+
+export default routes;
