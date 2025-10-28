dd3785458c2b725e70dd38d8b44dc334dadac516
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2025-10-28 16:06:34 +0800

    readability

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 // Routes
-import About from "pages/About";
 import LandingPage from "pages/LandingPage";
+import About from "pages/About";
 
 const routes = [
-  { name: "Home", route: "/", component: <LandingPage /> },
-  { name: "About", route: "/about", component: <About /> },
-  { name: "Services", route: "/services" },
+	{ name: "Home", route: "/", component: <LandingPage /> },
+  	{ name: "About", route: "/about", component: <About /> },
+  	{ 
+		name: "Services",
+    	collapse: [
+			{
+				name: "Investment Advisor",
+				route: "/services/investment-advisor"
+			}
+		]  
+	},
   { name: "Careers", route: "/careers" },
 ];
 
